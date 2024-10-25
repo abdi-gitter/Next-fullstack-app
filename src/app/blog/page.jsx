4b2886a53de82a3a0427590d23ce775fd3a5b0f8
abdi-gitter <abdisat.bedada@gmail.com>
2024-10-24 22:50:34 -0500

    Update

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { items } from './[id]/data'; // Ensure this path is correct
+import { items } from './[id]/data'; // Ensure this path is located in the `data` folder and is correct
 import styles from './page.module.css';
 
 const Blog = () => {
